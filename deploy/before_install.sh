72939ca0d233b993d8d9a20f92dcc4b2dd53af09
Evan Gentis <evan.gentis@gmail.com>
2018-11-07 10:29:44 -0500

    test

--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 yum update
+
+yum install ruby -yum
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+
 yum install -y httpd
-service httpd start
+systemctl start httpd
+systemctl enable httpd
+
+amazon-linux-extras install php7.2
+yum install mysql php-cli php-cgi php-gd -y
+
+chown -R apache:apache /var/www
+chmod -R g+rwx /var/www
