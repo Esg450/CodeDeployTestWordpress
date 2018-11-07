cc656ee0274c2d58b79adf93aa37a75b4805bb1b
Evan Gentis <evan.gentis@gmail.com>
2018-11-07 11:46:23 -0500

    Fix scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 yum install -y httpd
-systemctl enable httpd
 
 amazon-linux-extras install php7.2 -y
 yum install mysql php-cli php-cgi php-gd -y
+systemctl restart httpd
 
 chown -R apache:apache /var/www
 chmod -R g+rwx /var/www
